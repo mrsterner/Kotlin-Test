74bccf38091c011b9b4a2d91c844d2fa99e28f43
BuildTools <unconfigured@null.spigotmc.org>
2021-01-10 03:37:08 +0100

    Updated #1

--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -1,5 +1,20 @@
 fun sayHello(greeting:String, itemToGreet:String) = println("$greeting $itemToGreet")
 
-fun main(){
+fun main() {
     sayHello("Hey","Kotlin")
+
+    print("Enter: ")
+
+    val intInput = readLine()!!.toInt()
+    println("You entered: $intInput")
+    val listOfPeople = mutableListOf<String>()
+    for (i in 1..intInput) {
+        val person = readLine()!!
+        listOfPeople.add(person)
+    }
+    val newList = listOfPeople.shuffled()
+    val result = List(2) { mutableListOf<String>() }
+    newList.forEachIndexed { index, item -> result[index % 2].add(item) }
+    println(result)
+
 }
